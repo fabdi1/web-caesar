9947e83951fc6297a102aac3783896e142c9125a
fabdi1 <fma314@gmail.com>
2017-09-26 17:52:00 -0500

    encoded string to h1 tag

--- main.py
+++ main.py
@@ -1,10 +1,77 @@
-from flask import Flask
+from flask import Flask, request
+from caesar import rotate_string
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
+form= """
+<!doctype html>
+
+<html>
+    <head>
+        <style>
+            form {
+                background-color: #eee;
+                padding: 20px;
+                margin: 0 auto;
+                width: 540px;
+                font: 16px sans-serif;
+                border-radius: 10px;
+            }
+            textarea {
+                margin: 10px 0;
+                width: 540px;
+                height: 120px;
+            }
+        </style>
+    </head>
+    <body>
+    
+         <form method="post">
+            <div>
+                <label for="rot">Rotate by:</label>
+                <input type="text" name="rot" value="0">
+                <p class="error"></p>
+            </div>
+            <textarea type="text" name="text"></textarea>
+            <br>
+            <input type="submit">
+        </form>
+
+    </body>
+
+</html>
+
+"""
+
+
+@app.route("/", methods=["POST"])
+def encrypt():
+
+    rot = int(request.form[("rot")])
+    text =(request.form["text"])
+
+    encoded = rotate_string(text,rot)
+
+    return "<h1>" + encoded + "</h1>"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 @app.route("/")
 def index():
-    return "Hello World"
+    return form
 
 app.run()
